3cc55d0947728979ce7a0e37a970c5d41de8eaca
Joan Racenet <joan.racenet@hiptest.net>
2020-02-12 13:38:23 +0100

    Fix easy-datatable tests

--- tests/integration/components/easy-datatable-test.js
+++ tests/integration/components/easy-datatable-test.js
@@ -1,15 +1,12 @@
 import DatatableFactory from "ember-easy-datatable/utils/datatable-factory";
 import hbs from 'htmlbars-inline-precompile';
 import { moduleForComponent, test } from 'ember-qunit';
-import startApp from '../../helpers/start-app';
-import { run } from '@ember/runloop'
-
-var App;
+import customHelpers from '../../helpers/_custom-helpers'
+import $ from 'jquery'
 
 moduleForComponent('easy-datatable', 'Integration | Component | easy datatable', {
   integration: true,
-  setup: function() {
-    App = startApp();
+  setup() {
     this.set('table', DatatableFactory.makeDatatable({
     headers: ['Col 1', 'Col 2'],
     body: [
@@ -20,29 +17,27 @@ moduleForComponent('easy-datatable', 'Integration | Component | easy datatable',
     ]
   }));
   },
-  teardown: function() {
-    run(App, 'destroy');
-  }
-});
+})
 
-test('Basic test', function(assert) {
+test('Basic test', async function(assert) {
   assert.expect(1);
 
-  this.render(hbs`{{easy-datatable table=table}}`);
+  await this.render(hbs`{{easy-datatable table=table}}`);
 
-  assertDatatableContent(assert, [
+  assert.deepEqual(customHelpers.getDatatableContent(),[
     ['Row 11', 'Row 12'],
     ['Row 21', 'Row 22'],
     ['Row 31', 'Row 32'],
     ['Row 41', 'Row 42']
-  ]);
-});
+  ])
+})
 
-test('Can add class to table tag', function(assert) {
+test('Can add class to table tag', async function(assert) {
   assert.expect(2);
-  this.render(hbs`{{easy-datatable tableClasses='datatable table-bordered' table=table}}`);
+
+  await this.render(hbs`{{easy-datatable tableClasses='datatable table-bordered' table=table}}`)
 
   // by the user can add new ones
-  assert.ok(find('.datatable').length !== 0, 'the user can ...');
-  assert.ok(find('.table-bordered').length !== 0, '... add new classes for the table tage');
+  assert.ok($('.datatable').length !== 0, 'the user can ...');
+  assert.ok($('.table-bordered').length !== 0, '... add new classes for the table tage');
 });
