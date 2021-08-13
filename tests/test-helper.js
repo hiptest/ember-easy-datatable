21261c3f69839c06b85f83d568fef3d78ed43e66
vincent.capicotto <vincent.capicotto@hiptest.net>
2021-08-13 10:36:45 +0200

    upgrade to qunit 5

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,11 @@
 import Application from '../app'
 import config from '../config/environment'
 import { setApplication } from '@ember/test-helpers'
+import * as QUnit from 'qunit'
+import { setup } from 'qunit-dom'
 import { start } from 'ember-qunit'
+
+setup(QUnit.assert)
 
 setApplication(Application.create(config.APP))
 
